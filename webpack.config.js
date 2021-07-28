424d98bde21a93f7677085ea2f584f2c405c8bcc
Cyrus Xu <cyrus@overdose.digital>
2021-07-28 20:55:15 +1000

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.ts(x)?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1
+                        }
+                    },
+                    'postcss-loader'
+                ],
+                exclude: /\.module\.css$/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                            modules: true
+                        }
+                    },
+                    'postcss-loader'
+                ],
+                include: /\.module\.css$/
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./src/index.html"
+        }),
+    ],
+    resolve: {
+        extensions: [
+            '.tsx',
+            '.ts',
+            '.js'
+        ]
+    }
+};
+
+module.exports = config;
